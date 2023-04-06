d01df5d32273971a574a84713f1318d128115acf
Gangu, Anish <anishg2005@gmail.com>
2023-04-06 02:23:56 -0700

    cleaned up

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -61,10 +61,8 @@ public class SwerveModule {
   }
 
   public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
-    // Custom optimize command, since default WPILib optimize assumes continuous controller which
-    // REV and CTRE are not
+    // Custom optimize command, since default WPILib optimize assumes continuous controller which REV and CTRE are not
     desiredState = OnboardModuleState.optimize(desiredState, getState().angle);
-
     setAngle(desiredState);
     setSpeed(desiredState, isOpenLoop);
   }
